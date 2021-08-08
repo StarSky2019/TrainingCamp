771a1a752de87bd1843f68a7879176f0d7d3a0b3
Thinkbook <123456>
2021-08-08 12:26:37 +0800

    trainingCamp git commit

--- src/com/itheima/main/Main6.java
+++ src/com/itheima/main/Main6.java
@@ -2,6 +2,6 @@ package com.itheima.main;
 
 public class Main6 {
     public static void main(String[] args) {
-        System.out.println("新增main6");
+        System.out.println("新增Main6");
     }
 }
