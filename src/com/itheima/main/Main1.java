4e214e17eedad9b5e77affa098d395971bf4f451
Thinkbook <123456>
2021-08-13 20:53:32 +0800

    trainingCamp git commit

--- src/com/itheima/main/Main1.java
+++ src/com/itheima/main/Main1.java
@@ -12,25 +12,22 @@ public class Main1{
         for(int i=0; i<array.length; i++)
         {
             if(array[i] == '[')
-                stack.push(array[i]);
-            if(array[i] == '{')
-                stack.push(array[i]);
-            if(array[i] == '(')
-                stack.push(array[i]);
-            if(stack.isEmpty())
+                stack.push(']');
+            else if(array[i] == '{')
+                stack.push('}');
+            else if(array[i] == '(')
+                stack.push(')');
+            else if(stack.isEmpty() || array[i] != stack.pop())
             {
                 System.out.println(false);
                 return;
             }
-            else
-            {
-                char item = stack.pop();
-                if(item != array[i])
-                {
-                    System.out.println(false);
-                    return;
-                }
-            }
         }
+        if(stack.isEmpty())
+        {
+            System.out.println(true);
+            return;
+        }
+        System.out.println(false);
     }
 }
