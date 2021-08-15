af682733282963802d63299333fe49ba3e1de62d
Thinkbook <123456>
2021-08-15 17:28:57 +0800

    trainingCamp git commit

--- src/com/itheima/main/Main1.java
+++ src/com/itheima/main/Main1.java
@@ -4,21 +4,12 @@ import java.util.*;
 
 public class Main1{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        // 使用2种符号分隔字符串，并放到数组中去
         List<String> list = new ArrayList<>();
-        split(str, list);
-        // 打印出来
-        System.out.println(list);
-    }
-
-    public static void split(String s, List<String> l)
-    {
-        String[] array = s.split("[,&|]");
-        for(int i=0; i<array.length; i++)
-        {
-            l.add(array[i]);
+        list.add("xw");
+        list.add("xz");
+        list.add("xl");
+        for (String s : list) {
+            System.out.println(s);
         }
     }
 }
