15ad1ec64ea5aa98d4646c8b07f371e49b48e073
Thinkbook <123456>
2021-08-15 16:06:03 +0800

    trainingCamp git commit

--- src/com/itheima/main/Main1.java
+++ src/com/itheima/main/Main1.java
@@ -1,46 +1,24 @@
 package com.itheima.main;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        // 技术点1：去输入的字符串双引号
-        str = str.replace("\"", "");
-        Map<String, Integer> map = new HashMap<>();
-        // 技术点2：使用map保存数据
-        map.put("lower", 0);
-        map.put("upper", 0);
-        map.put("number", 0);
-        map.put("other", 0);
-        for(int i=0; i<str.length(); i++)
+        // 使用2种符号分隔字符串，并放到数组中去
+        List<String> list = new ArrayList<>();
+        split(str, list);
+        // 打印出来
+        System.out.println(list);
+    }
+
+    public static void split(String s, List<String> l)
+    {
+        String[] array = s.split("[,&|]");
+        for(int i=0; i<array.length; i++)
         {
-            char ch = str.charAt(i);
-            if(ch >= 'a' && ch <= 'z')
-            {
-                map.put("lower", map.get("lower") + 1);
-            }
-            else if(ch >= 'A' && ch <= 'Z')
-            {
-                map.put("upper", map.get("upper") + 1);
-            }
-            else if(ch >= '0' && ch <= '9')
-            {
-                map.put("number", map.get("number") + 1);
-            }
-            else
-            {
-                map.put("other", map.get("other") + 1);
-            }
-        }
-        // 技术点3：遍历打印hashmap
-        for(Map.Entry<String, Integer> entry : map.entrySet())
-        {
-            System.out.println("Key:" + entry.getKey() + " Value: " + entry.getValue());
+            l.add(array[i]);
         }
     }
 }
